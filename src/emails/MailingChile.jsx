a062141dd545eb331159120385fd0caf64647482
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2024-08-16 11:11:31 -0300

    mas fotos

---
+++ src/emails/MailingChile.jsx
@@ -0,0 +1,378 @@
+import {
+  Body,
+  Font,
+  Container,
+  Head,
+  Heading,
+  Column,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Text,
+  Row,
+  Tailwind,
+} from "@react-email/components";
+
+import * as React from "react";
+
+const baseUrl = process.env.VERCEL_URL || "https://cuyanas.vercel.app";
+
+export const AcademiaMailing = () => (
+  <Tailwind>
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Titan+One&family=Ultra&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Preview>#Academia Numen</Preview>
+      <Body
+        className="bg-gray-100 "
+        tyle={{ fontFamily: "'Poppins', sans-serif" }}
+      >
+        <Link
+          href="https://www.academianumen.com.ar/marketingPolitico"
+          target="_blank"
+        >
+          <Container className="relative w-[648px] max-w-full bg-[#1D1B22] text-white flex flex-col gap-12">
+            <Text className="text-center p-12 max-w-full ">
+              <Link
+                href="https://www.academianumen.com.ar"
+                target="_blank"
+                className="text-white cursor-pointer flex justify-center items-center"
+              >
+                <Img
+                  className="w-1/2"
+                  src="static/logoAcademia.svg"
+                  width={600}
+                  alt="logo academia"
+                />
+              </Link>
+            </Text>
+
+            <Heading
+              className={`flex justify-center items-center flex-col w-full `}
+            >
+              <Text
+                className="text-4xl text-center tracking-wider font-bold"
+                style={{ fontFamily: "'Ultra', serif" }}
+              >
+                DIPLOMATURA
+              </Text>
+              <Text
+                className="text-4xl tracking-wider font-bold"
+                style={{ fontFamily: "'Ultra', serif" }}
+              >
+                INTERNACIONAL
+              </Text>
+            </Heading>
+
+            <Section className="flex flex-col justify-center">
+              <Text className="text-white cursor-pointer flex justify-center items-center">
+                <Img
+                  className="w-10/12 mx-auto"
+                  src="static/logoMktpolitico.svg"
+                  width={600}
+                  alt="logoMktPolitico"
+                />
+              </Text>
+              <Text className="w-10/12 mx-auto">
+                <Img
+                  className="w-full"
+                  src="static/fernandoCerimedo.svg"
+                  width={200}
+                  alt="logo Profesor"
+                />
+              </Text>
+            </Section>
+            <Section
+              style={borderStyle}
+              className="w-9/12 mx-auto py-2 mb-8 rounded-3xl"
+            >
+              <Text
+                style={{ fontFamily: "'Poppins', sans-serif" }}
+                className="text-3xl text-center font-extrabold"
+              >
+                Inicio{" "}
+                <span className="text-[#A673F7] font-extrabold">
+                  7 de Octubre*
+                </span>
+              </Text>
+              <Text
+                style={{ fontFamily: "'Poppins', sans-serif" }}
+                className="text-xl text-center italic text-[#A673F7] -mt-5"
+              >
+                *cupos limitados
+              </Text>
+            </Section>
+
+            <Section className="w-10/12 mx-auto mb-12">
+              <Text
+                className="text-3xl text-center font-semibold mb-5"
+                style={{ fontFamily: "'Poppins', sans-serif" }}
+              >
+                Estrategia de campaña
+              </Text>
+
+              <Text
+                className="text-3xl text-center font-semibold mb-5"
+                style={{ fontFamily: "'Poppins', sans-serif" }}
+              >
+                Comunicación de gestión
+              </Text>
+
+              <Text
+                className="text-3xl text-center font-semibold mb-5"
+                style={{ fontFamily: "'Poppins', sans-serif" }}
+              >
+                Formación de candidatos y espacios políticos
+              </Text>
+
+              <Text
+                className="text-3xl text-center font-semibold "
+                style={{ fontFamily: "'Poppins', sans-serif" }}
+              >
+                El curso más completo y efectivo de Latinoamérica
+              </Text>
+            </Section>
+            <Section className="w-10/12 mx-auto mb-12 flex justify-around">
+              <Row>
+                <Column style={{ width: "16.666667%" }}>
+                  <Img
+                    src="static/tiempoIcon.svg"
+                    width={108}
+                    alt="pointer"
+                    className="w-full"
+                  />
+                </Column>
+                <Column style={columnStyle}>
+                  <Text
+                    style={{ fontFamily: "'Titan One', cursive" }}
+                    className="text-4xl border"
+                  >
+                    Duración <span className="text-[#C1FFD7]">8 meses</span>
+                  </Text>
+                </Column>
+              </Row>
+            </Section>
+            <Section style={sectionStyle}>
+              <Row>
+                <Column style={{ width: "16.666667%" }}>
+                  <Img
+                    style={imgStyle}
+                    src="static/itemIcon.svg"
+                    width={108}
+                    alt="pointer"
+                  />
+                </Column>
+                <Column style={columnStyle}>
+                  <Text style={paragraphStyle} className="text-xl font-bold">
+                    48 clases en vivo (Zoom) con presencialidad optativa y 24
+                    clases grabadas.
+                  </Text>
+                </Column>
+              </Row>
+              <Row>
+                <Column style={{ width: "16.666667%" }}>
+                  <Img
+                    style={imgStyle}
+                    src="static/itemIcon.svg"
+                    width={108}
+                    alt="pointer"
+                  />
+                </Column>
+                <Column style={columnStyle}>
+                  <Text style={paragraphStyle} className="text-xl font-bold">
+                    +60 horas de videos explicativos
+                  </Text>
+                </Column>
+              </Row>
+              <Row>
+                <Column style={{ width: "16.666667%" }}>
+                  <Img
+                    style={imgStyle}
+                    src="static/itemIcon.svg"
+                    width={108}
+                    alt="pointer"
+                  />
+                </Column>
+                <Column style={columnStyle}>
+                  <Text style={paragraphStyle} className="text-xl font-bold">
+                    Acceso ilimitado
+                  </Text>
+                </Column>
+              </Row>
+              <Row>
+                <Column style={{ width: "16.666667%" }}>
+                  <Img
+                    style={imgStyle}
+                    src="static/itemIcon.svg"
+                    width={108}
+                    alt="pointer"
+                  />
+                </Column>
+                <Column style={columnStyle}>
+                  <Text style={paragraphStyle} className="text-xl font-bold">
+                    +50 presentaciones descargables
+                  </Text>
+                </Column>
+              </Row>
+              <Row>
+                <Column style={{ width: "16.666667%" }}>
+                  <Img
+                    style={imgStyle}
+                    src="static/itemIcon.svg"
+                    width={108}
+                    alt="pointer"
+                  />
+                </Column>
+                <Column style={columnStyle}>
+                  <Text style={paragraphStyle} className="text-xl font-bold">
+                    Campus virtual
+                  </Text>
+                </Column>
+              </Row>
+              <Row>
+                <Column style={{ width: "16.666667%" }}>
+                  <Img
+                    style={imgStyle}
+                    src="static/itemIcon.svg"
+                    width={108}
+                    alt="pointer"
+                  />
+                </Column>
+                <Column style={columnStyle}>
+                  <Text className="text-xl font-bold" style={paragraphStyle}>
+                    Orientación y guía del profesor
+                  </Text>
+                </Column>
+              </Row>
+              <Row>
+                <Column style={{ width: "16.666667%" }}>
+                  <Img
+                    style={imgStyle}
+                    src="static/itemIcon.svg"
+                    width={108}
+                    alt="pointer"
+                  />
+                </Column>
+                <Column style={columnStyle}>
+                  <Text style={paragraphStyle} className="text-xl font-bold">
+                    Acceso desde PC o Celular
+                  </Text>
+                </Column>
+              </Row>
+              <Row>
+                <Column style={{ width: "16.666667%" }}>
+                  <Img
+                    style={imgStyle}
+                    src="static/itemIcon.svg"
+                    width={108}
+                    alt="pointer"
+                  />
+                </Column>
+                <Column style={columnStyle}>
+                  <Text style={paragraphStyle} className="text-xl font-bold">
+                    Certificación oficial: «Diplomatura en Marketing Político y
+                    comunicación de gestión»
+                  </Text>
+                </Column>
+              </Row>
+            </Section>
+            <Section style={{ padding: "20px" }}>
+              <Row>
+                <Column>
+                  <div
+                    className="w-9/12 mx-auto"
+                    style={{
+                      borderBottom: "2px dashed #C1FFD7",
+                      margin: "40px 0",
+                    }}
+                  />
+                </Column>
+              </Row>
+            </Section>
+            <Section>
+              <Text
+                style={{ fontFamily: "'Ultra', serif" }}
+                className="text-4xl font-semibold text-[#A673F7] text-center "
+              >
+                Medios de pago
+              </Text>
+            </Section>
+            <Section className="flex  justify-center">
+              <Img
+                src="static/mercadoPago.svg"
+                width={400}
+                alt="pointer"
+                className="my-12 block mx-auto w-2/3"
+              />
+              <Img
+                src="static/payPal.svg"
+                width={400}
+                alt="pointer"
+                className="my-12 block mx-auto w-2/3"
+              />
+              <Img
+                src="static/webPay.svg"
+                width={400}
+                alt="pointer"
+                className="my-12 block mx-auto w-1/3"
+              />
+              <Text
+                style={{ fontFamily: "'Poppins', sans-serif" }}
+                className="text-2xl w-9/12 mx-auto font-black  text-center mb-12"
+              >
+                Transferencia Bancaria Banco de Chile
+              </Text>
+            </Section>
+            <Text className="text-center p-12 max-w-full ">
+              <Link
+                href="https://www.academianumen.com.ar"
+                target="_blank"
+                className="text-white cursor-pointer flex justify-center items-center"
+              >
+                <Img
+                  className="w-1/2"
+                  src="static/logoAcademia.svg"
+                  width={600}
+                  alt="logo academia"
+                />
+              </Link>
+            </Text>
+          </Container>
+        </Link>
+      </Body>
+    </Html>
+  </Tailwind>
+);
+
+export default AcademiaMailing;
+
+const paragraphStyle = {
+  color: "#C1FFD7",
+  fontFamily: "Poppins, Arial, sans-serif",
+};
+
+const imgStyle = {
+  display: "block",
+  margin: "0 auto",
+  width: "32px",
+};
+
+const columnStyle = {
+  width: "80%",
+  margin: "0 auto",
+  textAlign: "left",
+};
+
+const sectionStyle = {
+  width: "83.333333%",
+  margin: "0 auto",
+};
+const borderStyle = {
+  border: "3px solid #C1FFD7",
+};
